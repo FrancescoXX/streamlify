9de3f4f5dd0b6d0131ad798cc3b63ff5e358e5cb
FrancescoXX <francescociullaroma@gmail.com>
2025-03-25 15:34:53 +0100

    removed eslint

--- src/app/api/flush/route.ts
+++ src/app/api/flush/route.ts
@@ -2,6 +2,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { flushIdeas } from "../../../lib/ideas";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(_request: NextRequest) {
   const updatedIdeas = flushIdeas();
   return NextResponse.json(updatedIdeas, { status: 200 });
